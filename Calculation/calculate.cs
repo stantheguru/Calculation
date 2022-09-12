f422b5d7a7e99e21c425700a5028bd973d1d5d0f
smatuku <smatuku@dewcis.com>
2022-09-12 11:25:17 +0300

    first commit

---
+++ Calculation/calculate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculation
+{
+    public class calculate
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
